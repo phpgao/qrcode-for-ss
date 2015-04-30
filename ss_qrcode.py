317ac2c3792a3e0d7ebfcfb9a171e9deaa0e9545
endoffight <endoffight@qq.com>
2015-04-30 09:20:58 +0800

    first commit

---
+++ ss_qrcode.py
@@ -0,0 +1,68 @@
+from flask import Flask,send_file
+import StringIO
+import base64
+import io
+import re
+import urllib2
+import qrcode
+
+is_debug = False
+
+path = dict()
+path['method'] = '/root/.kiwivm-shadowsocks-encryption'
+path['password'] = '/root/.kiwivm-shadowsocks-password'
+path['port'] = '/root/.kiwivm-shadowsocks-port'
+
+app = Flask(__name__)
+
+@app.route("/")
+def generateQRImage():
+
+    qr = qrcode.QRCode(version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+            )
+    qr.add_data(gen_ss_code())
+    qr.make(fit=True)
+
+    # creates qrcode base64
+    s_io = StringIO.StringIO()
+    qr_img = qr.make_image()
+    qr_img.save(s_io)
+
+    #response = Flask.make_response(s_io.getvalue())
+    #response.headers["Content-Type"] = "image/jpeg"
+    #response.headers["Content-Disposition"] = "attachment; filename=image.jpg"
+
+    return send_file(io.BytesIO(s_io.getvalue()),"image/png")
+    #return send_file('asdasdasd',"image/png")
+
+
+def get_ss_info(file=True):
+    config = {}
+    config['ip'] = urllib2.urlopen('http://ip.42.pl/raw').read()
+    if file:
+        with open(path['method']) as f:
+            config['method'] = f.readline().strip('\n')
+        with open(path['password']) as f:
+            config['password'] = f.readline().strip('\n')
+        with open(path['port']) as f:
+            config['port'] = f.readline().strip('\n')
+    else:
+        pass
+
+    return gen_config_str(config)
+
+def gen_config_str(config):
+    return "%s:%s@%s:%s" % (config['method'], config['password'], config['ip'], config['port'])
+
+def gen_ss_code(transfer_to_qrcode=True):
+    config_string = get_ss_info()
+    if transfer_to_qrcode:
+        config_string = base64.b64encode(config_string)
+        config_string = "ss://" + config_string
+    return config_string
+
+if __name__ == '__main__':
+    app.run(debug=is_debug,host='0.0.0.0', port=9537)
